8c1dcdf0e1ae77fab7dafc3c611d44cc9989954b
Shilpi <Shilpi@10.0.0.134>
2025-02-04 22:28:16 -0500

    Sample push for Git

---
+++ DataDriven/src/test/java/GitSample/NewGitSample.java
@@ -0,0 +1,12 @@
+package GitSample;
+
+import org.testng.annotations.Test;
+
+@Test
+public class NewGitSample {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
